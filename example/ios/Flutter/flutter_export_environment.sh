55e42d9508e6237bfbae18562b1f575850e2b2f4
Reni Delonzek <renidelonzek@gmail.com>
2022-10-15 14:41:20 -0300

    upd

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/reni/Downloads/Programas/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/reni/Downloads/Projetos/Flutter/libs/flutter_typeahead/example"
+export "FLUTTER_ROOT=/home/remessaonline/snap/flutter/common/flutter"
+export "FLUTTER_APPLICATION_PATH=/home/remessaonline/Downloads/Projects/Inchats/flutter_typeahead/example"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
 export "DART_OBFUSCATION=false"
-export "TRACK_WIDGET_CREATION=false"
+export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
-export "PACKAGE_CONFIG=.packages"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
